b16254e98ec239d715bc634194cd66b3f3b04f78
Swar <swar@pop-os.localdomain>
2022-11-06 06:53:33 +0530

    mid commit

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,9 +1,28 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+const style =
+{
+    wrapper : `flex-8 h-full w-full items-center `
+}
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 const Map = () => {
+
+  useEffect(()=>{
+      const map = new mapboxgl.Map({
+      container : 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 4,
+    })
+  },[])
+
   return (
-    <div>Map</div>
+    <div className={style.wrapper} id='map'>Map</div> 
+    // {/* here you have to give the id as map and the same value to cintaner */}
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from "../components/Navbar"
 import Map from "../components/Map"
+import LocationSelector from "../components/LocationSelector"
 const style = {
-  wrapper: `h-screen w-screen flex felx-col`
+  wrapper: `h-screen w-screen flex felx-col`,
+  main : `h-full w-screen flex-1 z-10 `,
+  mapContainer : `felx-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 }
 
 export default function Home() {
@@ -17,7 +22,7 @@ export default function Home() {
 
        <div className={style.rideRequestContainer}>
           <div className={style.rideRequest}>
-             {/* location selector */}
+             <LocationSelector/>
              {/* confirm ride */}
           </div>
        </div>
